5f5e132a8f5a055f79626f6171458112f00aca03
Mohammad Murtaza <64928763+Murtaza-data@users.noreply.github.com>
2026-06-10 00:05:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma, FAISS
 from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
+from langchain.retrievers.ensemble import EnsembleRetriever
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage
