fb62050142e26c2886f1b24dfb68cf4627f30c4b
Mohammad Murtaza <64928763+Murtaza-data@users.noreply.github.com>
2026-06-10 01:32:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.retrievers import BM25Retriever
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage
