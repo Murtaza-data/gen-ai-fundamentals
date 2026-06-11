632556095049607c88646b5b6c40d1ee39d05eae
Mohammad Murtaza <64928763+Murtaza-data@users.noreply.github.com>
2026-06-11 18:09:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
